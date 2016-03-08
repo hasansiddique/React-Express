3ed742d928ab297edd3a97198db389ee0da697fe
Hasan Siddique <hasan.chauhan91@gmail.com>
2016-03-08 14:15:00 +0500

    First commit with First Iteration code

---
+++ app/components/groceryItemsList.jsx
@@ -0,0 +1,14 @@
+var React = require('react/addons');
+
+var GroceryList = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1> Grocery List </h1>
+            </div>
+        )
+    }
+});
+
+
+module.exports = GroceryList;
---
+++ app/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react/addons');
+
+var GroceryList = require('./components/GroceryItemsList.jsx');
+
+React.render(<GroceryList/>, app);
